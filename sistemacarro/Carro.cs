640227d666e11eb3a0c164c9b710ce69c01ee331
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-10-30 20:11:37 -0400

    sistema carro

---
+++ sistemacarro/Carro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace sistemacarro
+{
+    public class Carro
+    {
+
+        public string Nome{
+            get;set;
+        }
+
+        public string Cor
+        {
+            get; set;
+        }
+
+        public string Marca
+        {
+            get; set;
+        }
+
+
+        public Carro()
+        {
+        }
+    }
+}
---
+++ sistemacarro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace sistemacarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            Console.WriteLine("Informe o nome do carro: ");
+            carro.Nome = Console.ReadLine();
+
+            Console.WriteLine("\nInforme a cor do carro: ");
+            carro.Cor = Console.ReadLine();
+
+            Console.WriteLine("\nInforme a marca do carro: ");
+            carro.Marca = Console.ReadLine();
+
+
+            Console.WriteLine("\n\nCarro {0} da cor {1} e da Marca {2}", carro.Nome, carro.Cor, carro.Marca);
+
+        }
+    }
+}
